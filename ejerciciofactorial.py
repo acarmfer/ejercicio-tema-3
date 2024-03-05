85c1d0e22acab1f4b8dab96927bc88eda27433f2
acarmfer <acarmfer@myuax.com>
2024-03-05 15:40:17 +0100

    n

--- ejerciciofactorial.py
+++ ejerciciofactorial.py
@@ -7,3 +7,26 @@ def factorial(n):
 
 n = 5
 print(factorial(n))
+
+#generame una funcion recursiva del facotrial de 5 
+def recursive_factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * recursive_factorial(n-1)
+
+print(recursive_factorial(5))
+
+# Llamar a ambas funciones
+def main():
+    n = 5
+    print(factorial(n))
+    print(recursive_factorial(n))
+
+if __name__ == "__main__":
+    main()
+    
+  
+
+    
+    
